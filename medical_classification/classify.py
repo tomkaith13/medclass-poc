6475c9c649ec4c6ea0b2216aeda25a5fc1fa97a4
Bibin Thomas <bthomas@league.com>
2025-04-29 13:28:15 -0400

    added first commit

---
+++ medical_classification/classify.py
@@ -0,0 +1,22 @@
+from typing import Literal
+import dspy
+
+class SpecialtyClassifier(dspy.Signature):
+    """Classify a wall of text to an appropriate medical specialty and confidence score. If nothing matches, return 'internal medicine' """
+
+    wall_of_text: str = dspy.InputField()
+   
+
+    specialty: Literal[ 'dermatology',
+        'orthopedics',
+        'cardiology',
+        'neurology',
+        'psychiatry',
+        'pediatrics',
+        'internal medicine',
+        'family medicine',
+        'emergency medicine',
+        'radiology'] = dspy.OutputField()
+    confidence: float = dspy.OutputField()
+
+specialty_classify = dspy.ChainOfThought(SpecialtyClassifier)
