2f5313b63a24f80bb0873d3790c34665b7d5a915
Bibin Thomas <bthomas@league.com>
2025-05-06 10:51:30 -0400

    added a title and description to the FastAPI server

--- main.py
+++ main.py
@@ -20,23 +20,22 @@ lm = dspy.LM(
 dspy.configure(lm=lm)
 dspy.settings.configure(track_usage=True)
 
-app = FastAPI()
+app = FastAPI(title="Medical Specialty Classification API",
+              description="This API classifies a wall of text to an appropriate medical specialty and confidence score.",
+              version="1.0.0",
+            )
 
 class ClassificationRequestBody(BaseModel):
     wall_of_text: str = Field(title="Wall of text to classify",
                               description="The wall of text to classify. This should be a string containing the text to be classified.",
                               default=None,
-                              example="Patient presents with a rash on the arm.")
+                              example="Patient presents with a rash on the arm.",
+                              min_length=10, max_length=2000)
 
 
 @app.post("/classify", status_code=status.HTTP_200_OK)
 def classify_handler( body: ClassificationRequestBody):
     wall_of_text = body.wall_of_text 
-    if not wall_of_text:
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content={"message": "wall_of_text is required."},
-        )
     
     #  Added some basic query validation
     if len(wall_of_text) > 2000 or len(wall_of_text) < 10:
