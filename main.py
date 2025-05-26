1c7e199bc681f46fd1011708b7b076024c1a07ae
Bibin Thomas <bthomas@league.com>
2025-05-25 20:00:12 -0400

    added one more sync tool

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ lm = dspy.LM(
     vertex_location=os.getenv("LOCATION"),
     temperature=0.1, 
     max_output_tokens=256,
-    cache=False,
+    cache=True,
 )
 dspy.configure(lm=lm)
 dspy.settings.configure(track_usage=True,async_max_workers=8 )
@@ -49,6 +49,13 @@ async def fetch_home_address_coordinates():
 
     return "12.34, -43.21"
 
+def fetch_work_address_coordinates():
+    """Fetch work address coordinates. If this fails, use home address coordinates using fetch_home_address_coordinates tool. """
+
+    # Uncomment the following line to simulate a failure
+    # raise Exception("Work address coordinates not found. Using home address coordinates instead.")
+    return "56.78, -87.65"
+
 async def query_run(query: str):
     print("Starting MCP client...")
     async with stdio_client(server_params) as (read, write):
@@ -63,7 +70,8 @@ async def query_run(query: str):
             for tool in tools.tools:
                 dspy_tools.append(dspy.Tool.from_mcp_tool(session, tool))
             
-            reactAgent = dspy.ReAct(UserQueryToLocationCoordinates, tools=dspy_tools + [fetch_home_address_coordinates])
+            reactAgent = dspy.ReAct(UserQueryToLocationCoordinates,
+                                    tools=dspy_tools + [fetch_home_address_coordinates, fetch_work_address_coordinates])
             
             result = await reactAgent.acall(query=query)
             # print(result)
@@ -77,7 +85,9 @@ app = FastAPI(title="Medical Specialty Classification API",
               )
 
 class UserQueryToLocationCoordinates(dspy.Signature):
-    """Convert a user query to location coordinates. If location is not found, call the fetch_home_coordinates tool and return that address."""
+    """Convert a user query to location coordinates. Use only values from the tools and NO OTHER.
+      If location is not found, call the fetch_work_coordinates tool and return that address.
+        If that fails as well, default to tool fetch_home_coordinates."""
 
     query: str = dspy.InputField()
     location_coordinates: str = dspy.OutputField(
@@ -111,6 +121,7 @@ async def classify_handler( body: ClassificationRequestBody):
 
     result = await query_run(query=wall_of_text)
     dspy.inspect_history(n=1)
+    print(f'cost of single-turn invocation: {lm.history[-1]["cost"]}')
     # print("results::", result)
     
     
